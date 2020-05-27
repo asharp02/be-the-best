f2ff07da943ece3a59ea326be318ac0079697b19
Adrian Sharpe <asharpe@nationaljournal.com>
2020-05-27 00:05:35 -0400

    build imdb scraper

---
+++ scraper.js
@@ -0,0 +1,28 @@
+const jsdom = require("jsdom");
+const fetch = require("isomorphic-fetch");
+
+const { JSDOM } = jsdom;
+const fs = require("fs");
+
+async function scrapeIMDB() {
+    const response = await fetch("https://www.imdb.com/title/tt0288937/trivia");
+    const text = await response.text();
+    const dom = await new JSDOM(text);
+    const triviaDivList = [
+        ...dom.window.document.querySelectorAll(".sodatext")
+    ];
+    triviaList = triviaDivList.map(triviaDiv => {
+        return triviaDiv.textContent;
+    });
+    await updateFile(triviaList);
+}
+
+scrapeIMDB();
+async function updateFile(trivia) {
+    console.log(trivia);
+    // triviaList = `[${trivia.toString()}]`;
+    fs.writeFile("trivia.json", trivia, function(err) {
+        if (err) throw err;
+        console.log("Updated");
+    });
+}
