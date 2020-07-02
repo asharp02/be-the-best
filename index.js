5d9d9f589925eeb0eeec16edaf7cdd1fe27d79c3
Adrian Sharpe <asharpe@nationaljournal.com>
2020-07-01 21:47:25 -0400

    add error handling

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ addEventListener("fetch", event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-    const endpoint =
-        "https://raw.githubusercontent.com/asharp02/be-the-best/master/trivia.json";
-    const response = await fetch(endpoint);
-    const triviaData = await response.json();
-    random = Math.floor(Math.random() * triviaData.length);
-    triviaJSON = JSON.stringify({ trivia: triviaData[random] });
-    return new Response(triviaJSON, { status: 200 });
+    try {
+        const endpoint =
+            "https://raw.githubusercontent.com/asharp02/be-the-best/master/trivia.json";
+        const response = await fetch(endpoint);
+        const triviaData = await response.json();
+        random = Math.floor(Math.random() * triviaData.length);
+        triviaJSON = JSON.stringify({ trivia: triviaData[random] });
+        return new Response(triviaJSON, { status: 200 });
+    } catch (err) {
+        return new Response("An error occurred", { status: 500 });
+    }
 }
